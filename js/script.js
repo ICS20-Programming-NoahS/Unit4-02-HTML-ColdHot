79768f33395b87a1eef42cd8ea9ff943dad5deaa
NoahSmith04 <124379601+NoahSmith04@users.noreply.github.com>
2023-04-19 04:05:22 +0000

    done

--- js/script.js
+++ js/script.js
@@ -14,12 +14,13 @@ function hotcold() {
   let result = "";
   let temperature = parseFloat(document.getElementById("hot-cold").value);
   
+  //calculates if the temperature is hot or cold
   if (temperature >= 5) {
     result = "It is hot!";
   }
   else {
     result = "It is cold!";
   }
-  
+  // Displays if the temperature is hot or cold back to user
   document.getElementById("result").innerHTML = result;
 }
